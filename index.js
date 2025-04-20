9293b58c865f3a215f42ae0f5ced0717a17bde55
Mason Ward <masonward2345@gmail.com>
2025-04-20 21:28:24 +0100

    Random statement complete

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 //Random message arrays that will make up the sentences.
 randArray1 = ['You', 'He', 'She', 'Them', 'I']
 randArray2 = ['look', 'are', 'seem', 'must be','is'];
-randArray3 = ['beautiful', 'pretty', 'gorgous', 'smart', 'amazing'];
+randArray3 = ['beautiful', 'pretty', 'gorgeous', 'smart', 'amazing'];
 
 
 let randomWord1 = Math.floor(Math.random() * 5);
-let randomWord2 = Math.floor(Math.random() * 5);
-let randomWord3 = Math.floor(Math.random() * 5);
+//let randomWord2 = Math.floor(Math.random() * 5);
+//let randomWord3 = Math.floor(Math.random() * 5);
 
+let finalArray = [];
+
+finalArray.push(randArray1[randomWord1], randArray2[randomWord1], randArray3[randomWord1]);
+console.log(finalArray);
