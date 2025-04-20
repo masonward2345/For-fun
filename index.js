432e8b43bedd75d03a3c5710a1799060741629a6
Mason Ward <masonward2345@gmail.com>
2025-04-20 20:50:56 +0100

    Adding relevent files

---
+++ index.js
@@ -0,0 +1,6 @@
+//Random message arrays that will make up the sentences.
+randArray1 = ['You', 'He', 'She', 'Them', 'I']
+randArray2 = ['look', 'are', 'seem', 'must be','is'];
+randArray3 = ['beautiful', 'pretty', 'gorgous', 'smart', 'amazing'];
+
+
